22bdb071dd9f353545f6de23d435a1ea19b79707
otodreas <159846818+otodreas@users.noreply.github.com>
2025-12-02 21:07:48 +0100

    cleaned up

--- scripts/d1/SafeSolver.py
+++ scripts/d1/SafeSolver.py
@@ -1,21 +1,13 @@
-# Import libraries
 import math
 
-# Define class
 class SafeSolver:
     def __init__(self, n_vals = 100):
         self.rng = list(range(n_vals))
 
     def position_looper(self, p, step):
-        # Determine how many "clicks" away from the starting position you end up
-        clicks = p + step
-        
-        # Determine how many cycles through the range have been completed
-        cycles = math.floor(clicks/len(self.rng))
-        
-        # Adjust the position by "resetting" the position at every cycle
-        p_new = clicks - cycles * len(self.rng)
-
+        clicks = p + step  # Determine how many "clicks" away from the starting position you end up
+        cycles = math.floor(clicks/len(self.rng))  # Determine how many cycles through the range have been completed
+        p_new = clicks - cycles * len(self.rng)  # Adjust the position by "resetting" the position at every cycle
         return p_new
 
     def solver(self, data, start_pos = 50):
@@ -25,16 +17,9 @@ class SafeSolver:
         answer = 0
 
         for d in data:
-            # Get increment from row in data
             increment = int(d[1:])
-            
-            # Get sign of increment
             direction = 1 if d[0] is "R" else -1
-            
-            # Update the position using position_looper
             position = self.position_looper(position, increment * direction)
-            
-            # Update answer if position is 0
             if position == 0:
                 answer += 1
 
