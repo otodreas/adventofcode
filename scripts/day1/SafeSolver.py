f970b24ecde62643eafb1ca5fe447a0afa61c46e
otodreas <159846818+otodreas@users.noreply.github.com>
2025-12-03 15:12:04 +0100

    formatting

--- scripts/day1/SafeSolver.py
+++ scripts/day1/SafeSolver.py
@@ -1,14 +1,18 @@
 import math
 
+
 class SafeSolver:
-    def __init__(self, method, n_vals = 100):
+    def __init__(self, method, n_vals=100):
         self.method = method
         self.rng = list(range(n_vals))
 
     def position_looper(self, p, increment, direction):
-        clicks = p + increment * direction  # Determine how many "clicks" away from the starting position you end up
-        cycles = self.__cycle_helper(clicks, self.rng)  # Determine how many cycles through the range have been completed
-        p_new = clicks - cycles * len(self.rng)  # Adjust the position by "resetting" the position at every cycle
+        # Determine how many "clicks" away from the starting position you end up
+        clicks = p + increment * direction
+        # Determine how many cycles through the range have been completed
+        cycles = self.__cycle_helper(clicks, self.rng)
+        # Adjust the position by "resetting" the position at every cycle
+        p_new = clicks - cycles * len(self.rng)
 
         # Count number of zeros passed, ruling out any instance of landing on zero
         if direction > 0:
@@ -21,7 +25,7 @@ class SafeSolver:
 
         return [p_new, zeros_passed]
 
-    def solver(self, data, start_pos = 50):
+    def solver(self, data, start_pos=50):
         self.data = data
         self.start_pos = start_pos
         position = self.start_pos
@@ -30,13 +34,15 @@ class SafeSolver:
         for d in data:
             increment = int(d[1:])
             direction = 1 if d[0] == "R" else -1
-            position, zeros_passed = self.position_looper(position, increment, direction)
+            position, zeros_passed = self.position_looper(
+                position, increment, direction
+            )
             if position == 0:
-                    answer += 1  # Update zeros landed on
+                answer += 1  # Update zeros landed on
             if self.method == 2:
                 answer += zeros_passed  # Update zeros passed
 
         return answer
-    
+
     def __cycle_helper(self, clicks, rng):
-        return math.floor(clicks/len(rng))
+        return math.floor(clicks / len(rng))
--- scripts/day2/idParser.py
+++ scripts/day2/idParser.py
@@ -1,27 +1,4 @@
-raw_data = "18623-26004,226779-293422,65855-88510,868-1423,248115026-248337139,903911-926580,97-121,67636417-67796062,24-47,6968-10197,193-242,3769-5052,5140337-5233474,2894097247-2894150301,979582-1016336,502-646,9132195-9191022,266-378,58-91,736828-868857,622792-694076,6767592127-6767717303,2920-3656,8811329-8931031,107384-147042,941220-969217,3-17,360063-562672,7979763615-7979843972,1890-2660,23170346-23308802"
-
 import math
-
-
-xxx = [111111, 222222]
-xx = range(xxx[0], xxx[1] + 1)
-
-# xx = (range(1234, 1236))
-
-# print(len(x)%2)
-
-# for x in xx:
-#     x = str(x)
-#     for i in range(2, math.floor(len(x)/2)+1):        
-#         if len(x)%i == 0:           
-
-#             slices = []
-#             for j in range(int(len(x)/i)):
-#                 slices.append(x[j*i:(j+1)*i])
-#             if len(set(slices)) == 1:
-#                 print(x)
-#                 break
-
 
 
 class idParser:
@@ -40,8 +17,8 @@ class idParser:
             values.append(range(pair[0], pair[1] + 1))
 
         return values
-    
-    def parse1(self, values, tb = False):
+
+    def parse1(self, values, tb=False):
         self.values = values
         self.tb = tb
         sum_invalid_ids = 0
@@ -51,7 +28,7 @@ class idParser:
             for id in ids:
                 id = str(id)
                 if len(id) % 2 == 0:
-                    midpoint = int(len(id)/2)
+                    midpoint = int(len(id) / 2)
                     front = id[:midpoint]
                     back = id[midpoint:]
                     if front == back:
@@ -60,7 +37,7 @@ class idParser:
                             print(int(id))
 
         return sum_invalid_ids
-    
+
     def parse2(self, values, tb=False):
         self.values = values
         self.tb = tb
@@ -70,83 +47,15 @@ class idParser:
             ids = list(pair)
             for id in ids:
                 id = str(id)
-                for i in range(1, math.floor(len(id)/2)+1):
+                for i in range(1, math.floor(len(id) / 2) + 1):
                     if len(id) % i == 0:
                         slices = []
-                        for j in range(int(len(id)/i)): 
-                            slices.append(id[j*i:(j+1)*i])
+                        for j in range(int(len(id) / i)):
+                            slices.append(id[j * i : (j + 1) * i])
                         if len(set(slices)) == 1:
                             sum_invalid_ids += int(id)
                             if tb:
                                 print(int(id))
                             break
 
-        return(sum_invalid_ids)                         
-
-
-
-
-
-# for x in xx:
-#     x = str(x)
-#     for i in range(1, math.floor(len(x)/2)+1):
-#         if len(x)%i == 0:
-#             slices = []
-#             for j in range(int(len(x)/i)):
-#                 slices.append(x[j*i:(j+1)*i])
-#             if len(set(slices)) == 1:
-#                 print(x)
-#                 break
-
-# print(x[:f])
-# print(x[f:])
-# for i in range(1, (math.floor(len(x)/2)) + 1):
-#     for j in range()
-    
-    
-#     y = x[0:i]
-#     z = x[i:i*2]
-#     if y == z:
-#         print(y, z, "match")
-#     else:
-#         print(y, z)
-
-# raw_data_list = raw_data.split(sep=",")
-
-# values = []
-# for d in raw_data_list:
-#     pair = d.split(sep="-")
-#     for i in [0, 1]:
-#         pair[i] = int(pair[i])
-#     values.append(range(pair[0], pair[1]))
-
-# sum_invalid_ids = 0
-
-# for pair in values:
-#     ids = list(pair)
-#     for id in ids:
-#         id = str(id)
-#         if len(id) % 2 == 0:
-#             midpoint = int(len(id)/2)
-#             front = id[:midpoint]
-#             back = id[midpoint:]
-#             if front == back:
-#                 sum_invalid_ids += int(id)
-
-
-
-# for pair in values:
-#     ids = list(pair)
-#     for id in ids:
-#         id = str(id)
-#         for i in range(2, math.floor(len(id)/2)+1):
-#             if len(id)%i == 0:
-#                 slices = []
-#                 for j in range(int(len(id)/i)):
-#                     slices.append(id[j*i:(j+1)*i])
-#                 if len(set(slices)) == 1:
-#                     sum_invalid_ids += int(id)
-#                     # print(id)
-#                     break
-
-# print(sum_invalid_ids)
+        return sum_invalid_ids
