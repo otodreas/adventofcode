ed3499010ace3d0e4211ccdec65b83ba2e135085
otodreas <159846818+otodreas@users.noreply.github.com>
2025-12-03 01:52:47 +0100

    completed part 2

--- scripts/SafeSolver.py
+++ scripts/SafeSolver.py
@@ -5,14 +5,19 @@ class SafeSolver:
         self.method = method
         self.rng = list(range(n_vals))
 
-    def position_looper(self, p, step):
-        clicks = p + step  # Determine how many "clicks" away from the starting position you end up
-        cycles = math.floor(clicks/len(self.rng))  # Determine how many cycles through the range have been completed
+    def position_looper(self, p, increment, direction):
+        clicks = p + increment * direction  # Determine how many "clicks" away from the starting position you end up
+        cycles = self.__cycle_helper(clicks, self.rng)  # Determine how many cycles through the range have been completed
         p_new = clicks - cycles * len(self.rng)  # Adjust the position by "resetting" the position at every cycle
 
-        zeros_passed = abs(cycles) #if p != 0 else abs(cycles) - 1
-        # if step <= 0 and p_new == 0 and zeros_passed > 1:
-        # print(f"{p} + {step} = {p_new}, {cycles} {zeros_passed}")
+        # Count number of zeros passed, ruling out any instance of landing on zero
+        if direction > 0:
+            zeros_passed = abs(self.__cycle_helper(clicks - 1, self.rng))
+        else:
+            if p != 0:
+                zeros_passed = abs(self.__cycle_helper(clicks, self.rng))
+            else:
+                zeros_passed = abs(self.__cycle_helper(clicks, self.rng)) - 1
 
         return [p_new, zeros_passed]
 
@@ -21,16 +26,17 @@ class SafeSolver:
         self.start_pos = start_pos
         position = self.start_pos
         answer = 0
-        step_previous = None
 
         for d in data:
             increment = int(d[1:])
             direction = 1 if d[0] == "R" else -1
-            position, zeros_passed = self.position_looper(position, increment * direction)
-            if self.method == 1:
-                if position == 0:
+            position, zeros_passed = self.position_looper(position, increment, direction)
+            if position == 0:
                     answer += 1
-            else:
+            if self.method == 2:
                 answer += zeros_passed
 
         return answer
+    
+    def __cycle_helper(self, clicks, rng):
+        return math.floor(clicks/len(rng))
--- scripts/main.py
+++ scripts/main.py
@@ -3,20 +3,13 @@ i = 0
 data = []
 with open("scripts/input.txt", "r") as f:
     while True:
-    # while i<5:
-        # i += 1
         line = f.readline().strip()
         if line:
             data.append(line)
         else:
             break
 
-solver = SafeSolver(1)
+method = 2
+solver = SafeSolver(method)
 answer = solver.solver(data)
 print(answer)
-# print(SafeSolver.position_looper(self=solver, p=1, step_prev=0, step=-1)[2])
-
-# 0 -> + : the starting 0 is NOT counted
-# 0 -> - : the starting 0 IS counted
-# + -> 0 : the ending 0 IS counted
-# - -> 0 : the ending 0 is NOT counted
