cd7e56a65d2fb9c91519b915d68ad51b1d3a1d1a
otodreas <159846818+otodreas@users.noreply.github.com>
2025-12-04 17:55:15 +0100

    clarity

--- scripts/day4/rollaccess.py
+++ scripts/day4/rollaccess.py
@@ -16,13 +16,18 @@ class RollAccess:
     def get_rolls(self, arr):
         accessible_rolls_count = 0
         arr_updated = np.copy(arr)
+        # Loop through each position, not including pad positions
         for i in range(1, np.shape(arr)[0] - 1):
             for j in range(1, np.shape(arr)[1] - 1):
+                # Check if the position contains a roll
                 if arr[i, j] == "@":
                     frame = np.array([arr[i - 1 : i + 2, j - 1 : j + 2]])
-                    kernel = np.array(
-                        [["@", "@", "@"], ["@", "X", "@"], ["@", "@", "@"]]
-                    )
+                    kernel = np.array([
+                        ["@", "@", "@"],
+                        ["@", "X", "@"],
+                        ["@", "@", "@"]
+                    ])
+                    # Update variables if there are 4 or more rolls around the position
                     if np.sum(frame == kernel) < 4:
                         accessible_rolls_count += 1
                         arr_updated[i, j] = "."
