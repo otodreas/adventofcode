b30ebd06eec07b895ce815c3349e8d2ed9b8d65b
otodreas <159846818+otodreas@users.noreply.github.com>
2025-12-04 17:51:29 +0100

    completed day 4

---
+++ scripts/day4/main4.py
@@ -0,0 +1,18 @@
+import numpy as np
+from rollaccess import RollAccess
+
+r = RollAccess()
+start_arr = r.make_matrix("scripts/day4/input4.txt")
+nrolls, arr = r.get_rolls(start_arr)
+print(nrolls)  # Print number of rolls accessed first time
+
+total_rolls_pulled = nrolls
+x = 0
+while True:
+    nrolls, arr_updated = r.get_rolls(arr)
+    total_rolls_pulled += nrolls
+    if not np.array_equal(arr, arr_updated):
+        arr = arr_updated
+    else:
+        break
+print(total_rolls_pulled)  # Print all possible rolls to get
---
+++ scripts/day4/rollaccess.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class RollAccess:
+    def __init__(self):
+        pass
+
+    def make_matrix(self, text):
+        with open(text, "r") as f:
+            lst = []
+            for line in f:
+                lst.append(list("." + line.strip() + "."))
+            pad = [list("." * len(lst[0]))]
+            return np.array(pad + lst + pad)
+
+    def get_rolls(self, arr):
+        accessible_rolls_count = 0
+        arr_updated = np.copy(arr)
+        for i in range(1, np.shape(arr)[0] - 1):
+            for j in range(1, np.shape(arr)[1] - 1):
+                if arr[i, j] == "@":
+                    frame = np.array([arr[i - 1 : i + 2, j - 1 : j + 2]])
+                    kernel = np.array(
+                        [["@", "@", "@"], ["@", "X", "@"], ["@", "@", "@"]]
+                    )
+                    if np.sum(frame == kernel) < 4:
+                        accessible_rolls_count += 1
+                        arr_updated[i, j] = "."
+
+        return accessible_rolls_count, arr_updated
