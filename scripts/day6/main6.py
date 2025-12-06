7616d455895f87bf8d6fa74b4c38253aa0778e0f
otodreas <159846818+otodreas@users.noreply.github.com>
2025-12-06 20:07:54 +0100

    finished day 6

---
+++ scripts/day6/main6.py
@@ -0,0 +1,24 @@
+import re
+from mathsolver import MathSolver
+
+
+solver = MathSolver()
+
+
+def solve(data, method):
+    with open(data, "r") as f:
+        data_list = []
+        for line in f:
+            data_list.append(line)
+        solver.prepare_arrays(data_list, method)
+        return solver.calculate_sum()
+
+
+def check(answer, key):
+    return "Too small" if answer < key else "Too big" if answer > key else "Solved"
+
+
+print(check(solve("scripts/day6/toy_input6.txt", 1), 4277556))
+print(solve("scripts/day6/input6.txt", 1))
+print(check(solve("scripts/day6/toy_input6.txt", 2), 3263827))
+print(solve("scripts/day6/input6.txt", 2))
---
+++ scripts/day6/mathsolver.py
@@ -0,0 +1,42 @@
+import re
+import numpy as np
+
+
+class MathSolver:
+    def __init__(self):
+        pass
+
+    def prepare_arrays(self, data_list, method):
+        # Assign operation symbols as attribute
+        self.operations = re.split(r"\s+", data_list[-1].strip())
+        if method == 2:
+            # Get individual characters including spaces from the number rows
+            digits_list = [(list(line.strip("\n"))) for line in data_list[:-1]]
+            # Transform data so that vertically conserved spaces are rows in an array
+            digits_arr = np.array(digits_list, dtype=np.str_).T
+            # Loop through rows of numerical data
+            numbers_list = []
+            numbers_row = []
+            for digits in digits_arr:
+                if np.sum(digits == " ") < len(digits):
+                    # Add numbers to a "row" in the list of numbers
+                    numbers_row.append(int("".join(digits).strip()))
+                else:
+                    # Add the "row" of numbers to the list of numbers and reassing the "row" list to an empty list
+                    numbers_list.append(numbers_row)
+                    numbers_row = []
+            # Append the final "row" of numbers to the list of numbers and assign as attribute
+            numbers_list.append(numbers_row)
+            self.numbers = numbers_list
+
+        else:
+            # Get numbers in each row of text
+            digits_list = [re.split(r"\s+", line.strip()) for line in data_list[:-1]]
+            # Create array, transform so that operations are performed on rows, assign as attribute
+            self.numbers = np.array(digits_list, dtype=np.int16).T
+
+    def calculate_sum(self):
+        sum = 0
+        for i, n in enumerate(self.numbers):
+            sum += np.sum(n) if self.operations[i] == "+" else np.prod(n)
+        return sum
