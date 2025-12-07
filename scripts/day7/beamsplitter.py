2845eec2554d5de80fdc42f40ac539f6463a4248
otodreas <159846818+otodreas@users.noreply.github.com>
2025-12-07 18:26:43 +0100

    created dummy

--- scripts/day7/beamsplitter.py
+++ scripts/day7/beamsplitter.py
@@ -3,19 +3,19 @@ import re
 
 class BeamSplitter:
     def __init__(self):
-        self.split_indices = set()
+        pass
 
     def find_splits(self, data_list, method):
         n_splits = 0
-        n_paths = 0
+        paths = []
         for line in data_list:
-            beam_indices = set([c.start(0) for c in re.finditer("[^.]", line.strip())])
+            split_indices = set([c.start(0) for c in re.finditer("[^.]", line.strip())])
             if "S" in line:
-                self.split_indices = beam_indices
+                beam_indices = split_indices
             else:
-                if len(beam_indices) > 0:
-                    for i in beam_indices & self.split_indices:
-                        self.split_indices.remove(i)
-                        self.split_indices.update((i - 1, i + 1))
+                if len(split_indices) > 0:
+                    for i in sorted(list(split_indices & beam_indices)):
+                        beam_indices.remove(i)
+                        beam_indices.update((i - 1, i + 1))
                         n_splits += 1
-        return n_splits if method == 1 else n_paths
+        return n_splits if method == 1 else len(paths)
